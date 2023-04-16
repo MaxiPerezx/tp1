f3dca16249ec1f85c5f9185b93d2916b2b18d820
MaxiPerezx <maxmanul@live.com>
2023-04-16 13:26:45 -0300

    tp1

---
+++ DoWhile.java
@@ -0,0 +1,16 @@
+package tp1;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		 int i = 1;
+	        System.out.println("Numeros del 1 al 100: (Usando Do While) ");                                                             
+	        do{
+	             System.out.println(i);
+	                   i++;
+	       
+	        }while(i<=100); 
+	        
+	}
+}
---
+++ While.java
@@ -0,0 +1,15 @@
+package tp1;
+
+public class While {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Numeros del 1 al 100: (Usando While)");   // imprimo por pantalla el msj                                                         
+        int i=1;
+        while(i<=100) {								// la condición booleana se evalúa antes de que se ejecute el código
+                  System.out.println(i);            // se ejecuta el proceso                                                       
+                  i++;}								// incremento y regreso al bucle
+	}
+
+
+}
